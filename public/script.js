57245e6dddf1fffd5508dff004fea317cfd1303d
SimonSmale <simon@smalemail.net>
2017-12-02 20:09:37 +0000

    added dummy items and the functionality to add them to the cart

--- public/script.js
+++ public/script.js
@@ -1,6 +1,25 @@
+const PRICE = 9.99
+
 new Vue({
     el: '#app',
     data: {
-        total: 0
+        total: 0,
+        items: [{
+                title: 'item1'
+            },
+            {
+                title: 'item2'
+            },
+            {
+                title: 'item3'
+            }
+        ],
+        cart: []
+    },
+    methods: {
+        addItem: function (index) {
+            this.total += PRICE
+            this.cart.push(this.items[index])
+        }
     }
 });
